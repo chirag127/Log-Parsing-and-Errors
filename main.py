439caf981f595599a48e470f958c3b2962137df6
Chirag <76880977+chirag127@users.noreply.github.com>
2023-09-26 11:25:18 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,114 +1,197 @@
-# Import the necessary libraries
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
-from sklearn.metrics.pairwise import euclidean_distances
-import pickle
+import re
 
-def my_print(x):
-    # my_print(x)
-    pass
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+import re
 
+# Load the log data from the CSV file (replace with your own dataset path)
+log_data = pd.read_csv("/workspaces/Log-Parsing-and-Errors/error.csv")
 
-# Load the log data from the CSV file
-log_data = pd.read_csv('/workspaces/Log-Parsing-and-Errors/error.csv')
-
-# remove all the columns except the message column i.e. _raw
-
-log_data = log_data[['_raw']]
-
-# Preprocess the log messages
-# Data cleaning: remove any non-alphanumeric characters and convert to lowercase
-log_data['_raw'] = log_data['_raw'].str.replace('[^a-zA-Z0-9 ]', '').str.lower()
-
-## randomise the order of the log messages
-log_data = log_data.sample(frac=1).reset_index(drop=True)
-
-# Tokenization: split the messages into words
-log_data['tokens'] = log_data['_raw'].str.split()
-
-# Vectorization: transform the tokens into numerical vectors using TF-IDF
-vectorizer = TfidfVectorizer(analyzer=lambda x: x) # Use the tokens as they are
-X = vectorizer.fit_transform(log_data['tokens']) # X is a sparse matrix of shape (n_samples, n_features)
-
-# Split the log data into training and testing datasets
-# Use an 80-20 split, where 80% of the data goes to training and 20% to testing
-train_size = int(0.8 * len(log_data)) # Calculate the number of samples for training
-train_data = log_data[:train_size] # Select the first train_size samples for training
-test_data = log_data[train_size:] # Select the remaining samples for testing
-
-# Determine the optimal number of clusters using the Elbow Method
-# You can adjust the range of cluster numbers as needed
-wcss = []  # Within-cluster sum of squares
-max_clusters = 10  # Maximum number of clusters to consider
-for i in range(1, max_clusters + 1):
-    kmeans = KMeans(n_clusters=i, random_state=0)
-    kmeans.fit(X[:train_size])
-    wcss.append(kmeans.inertia_)
-
-# Find the "elbow" point in the plot
-elbow_point = None
-for i in range(1, len(wcss) - 1):
-    if (wcss[i] - wcss[i + 1]) / (wcss[i - 1] - wcss[i]) > 0.1:
-        elbow_point = i + 1
-        break
-
-if elbow_point is None:
-    elbow_point = 1  # Default to 1 cluster if no clear elbow point
-
-# Train the final K-Means model with the determined number of clusters
-n_clusters = 5  # Number of clusters determined by the Elbow Method
-kmeans = KMeans(n_clusters=n_clusters, random_state=0)
-kmeans.fit(X[:train_size])
-
-# Save the trained K-Means model to a file for future use
-filename = 'log_pattern_model.pkl'  # File name to save the model
-pickle.dump(kmeans, open(filename, 'wb'))  # Save the model using pickle
-
-# Set a minimum probability threshold for pattern matching, say 0.3, and change it later if needed
-threshold = 0.3  # Minimum probability threshold for pattern matching
-
-# Predict log patterns for the testing data
-# Randomly select a few log messages from the testing data, say 5 messages to start
-np.random.seed(0)  # Set a random seed for reproducibility
-sample_size = 5  # Number of messages to select randomly
-sample_indices = np.random.choice(len(test_data), size=sample_size, replace=False)  # Randomly select sample_size indices from the test data
-sample_messages = test_data.iloc[sample_indices]['_raw']  # Select the corresponding messages from the test data
-
-# For each test log message:
-for message in sample_messages:
-    # Compute the distances of the message to all cluster centers
-    message_vector = vectorizer.transform([message])  # Transform the message into a vector using TF-IDF
-    distances = euclidean_distances(message_vector, kmeans.cluster_centers_)  # Compute the distances to all cluster centers using Euclidean distance
-
-    # Find the cluster with the minimum distance
-    min_distance = np.min(distances)  # Find the minimum distance among all distances
-    min_cluster = np.argmin(distances)  # Find the cluster index that corresponds to the minimum distance
-
-    # If the minimum distance is below the minimum probability threshold, assign the message to the cluster
-    if min_distance < threshold:
-        my_print(f'The message "{message}" belongs to cluster {min_cluster}')
-
-    # If the minimum distance is above the threshold, create a new cluster for the message
-    else:
-        my_print(f'The message "{message}" does not match any existing cluster')
-        n_clusters += 1  # Increment the number of clusters by 1
-        kmeans.n_clusters = n_clusters  # Update the number of clusters in the K-Means model
-        kmeans.cluster_centers_ = np.append(kmeans.cluster_centers_, message_vector.toarray(), axis=0)  # Append the message vector as a new cluster center
-
-# Print out the cluster numbers for the training data
-my_print(f'The cluster numbers for the training data are: {kmeans.labels_}')
+# Keep only the '_raw' column
+log_data = log_data[["_raw"]]
 
 
-# Print out the two example of each cluster
-for i in range(n_clusters):
-    try:
-        print(f'Cluster {i}:')
-        print(log_data.iloc[np.where(kmeans.labels_ == i)[0][0]]['_raw'])
-        print('the second one')
-        print(log_data.iloc[np.where(kmeans.labels_ == i)[0][1]]['_raw'])
+# # for each log message, remove the log messages which don't contain "ERROR" in the first 100 characters
+# for i in range(len(log_data)):
+#     if "ERROR" not in log_data["_raw"][i][:100]:
+#         log_data = log_data.drop(i)
 
-        print('')
-    except:
-        pass
+# Reset the index
+log_data = log_data.reset_index(drop=True)
+
+
+# for each log message, remove the everything before the third :
+for i in range(len(log_data)):
+    log_data["_raw"][i] = log_data["_raw"][i].split(":", 3)[-1]
+
+
+# for each log message, truncate the log messages after the first 100 characters
+
+for i in range(len(log_data)):
+    log_data["_raw"][i] = log_data["_raw"][i][:500]
+
+
+# for each log message, remove everything after the first line break character
+for i in range(len(log_data)):
+    log_data["_raw"][i] = log_data["_raw"][i].split("\n", 1)[0]
+
+
+# Data preprocessing: remove non-alphanumeric characters and convert to lowercase
+log_data["_raw"] = log_data["_raw"].str.replace("[^a-zA-Z0-9 ]", "").str.lower()
+
+# Define a regular expression pattern to match URLs
+url_pattern = (
+    r"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+)
+
+
+# Function to remove URLs from a text
+def remove_urls(text):
+    return re.sub(url_pattern, "", text)
+
+
+# Apply the remove_urls function to each log message
+log_data["_raw"] = log_data["_raw"].apply(remove_urls)
+
+data = {
+    "log_message": log_data["_raw"].tolist(),
+}
+
+df = pd.DataFrame(data)
+
+
+# Vectorize log messages using TF-IDF
+vectorizer = TfidfVectorizer(max_features=500, stop_words="english")
+log_vectors = vectorizer.fit_transform(df["log_message"])
+
+
+
+# find the optimal number of clusters
+# from sklearn.metrics import silhouette_score
+
+# # Calculate Silhouette Score for different values of k
+# silhouette_scores = []
+# for k in range(2, 21):  # You can adjust the range as needed
+#     kmeans = KMeans(n_clusters=k, random_state=42)
+#     cluster_labels = kmeans.fit_predict(log_vectors)
+#     silhouette_avg = silhouette_score(log_vectors, cluster_labels)
+#     silhouette_scores.append(silhouette_avg)
+
+
+#     # Find the optimal number of clusters
+# optimal_k = silhouette_scores.index(max(silhouette_scores)) + 2  # +2 because we started from k=2
+
+
+# Calculate WCSS for different values of k
+# wcss = []
+# for k in range(1, 21):  # You can adjust the range as needed
+#     kmeans = KMeans(n_clusters=k, random_state=42)
+#     kmeans.fit(log_vectors)
+#     wcss.append(kmeans.inertia_)
+
+# # Find the optimal number of clusters (k)
+# optimal_k = None
+# for i in range(1, len(wcss)):
+#     slope = (wcss[i] - wcss[i - 1]) / 2  # Calculate the slope of the WCSS curve
+#     if slope < 100:  # You can adjust this threshold based on your data
+#         optimal_k = i + 1  # +1 because we started from k=1
+#         break
+
+
+# Train a K-Means clustering model
+n_clusters = 5  # You can adjust the number of clusters as needed
+kmeans = KMeans(n_clusters=n_clusters)
+kmeans.fit(log_vectors)
+
+# Extract keywords from cluster centroids
+cluster_keywords = {}
+for cluster_id in range(n_clusters):
+    # Get indices of log messages in this cluster
+    cluster_indices = kmeans.labels_ == cluster_id
+
+    # Extract keywords from the log messages in this cluster
+    keywords = []
+    for log_message in df.loc[cluster_indices, "log_message"]:
+        keywords += re.findall(r"\b\w+\b", log_message)
+
+    # Count and rank keyword occurrences
+    keyword_counts = pd.Series(keywords).value_counts()
+
+    # Store the top keywords for this cluster
+    cluster_keywords[cluster_id] = keyword_counts.index.tolist()[
+        :5
+    ]  # Adjust the number of keywords as needed
+
+# Print the identified patterns (top keywords for each cluster)
+print("Identified Patterns:")
+for cluster_id, keywords in cluster_keywords.items():
+    print(f"Pattern {cluster_id + 1}: {', '.join(keywords)}")
+
+
+# make a list of one message per cluster
+cluster_messages = []
+for cluster_id in range(n_clusters):
+    # Get indices of log messages in this cluster
+    cluster_indices = kmeans.labels_ == cluster_id
+
+    # Extract keywords from the log messages in this cluster
+    keywords = []
+    for log_message in df.loc[cluster_indices, "log_message"]:
+        keywords += re.findall(r"\b\w+\b", log_message)
+
+    # Count and rank keyword occurrences
+    keyword_counts = pd.Series(keywords).value_counts()
+
+    # Store the top keywords for this cluster
+    cluster_messages.append(df.loc[cluster_indices, "log_message"].tolist()[0])
+
+
+# print(cluster_messages)
+for message in cluster_messages:
+    print(f"cluster number: {cluster_messages.index(message) + 1} \n {message} \n")
+# Replace 'YOUR_API_KEY' with your CohereAI API key
+import cohere
+
+co = cohere.Client(
+    "PQ50WPjjMsFSzUhZlMQaGTlS30MyRs9YkbuKfhHh"
+)  # This is your trial API key
+
+
+def generate_patterns(log_messages, num_patterns=5):
+    patterns = []
+
+    # Iterate through log messages
+    for message in log_messages:
+        response = co.generate(
+            model="command-nightly",
+            prompt="Recognize patterns in the following log message:\n\nLog Message: {}\n\nPatterns:".format(
+                message
+            ),
+            max_tokens=300,
+            temperature=0.9,
+            k=0,
+            stop_sequences=[],
+            return_likelihoods="NONE",
+        )
+
+        print("Prediction: {}".format(response.generations[0].text))
+        # Extract generated patterns from the response
+        generated_patterns = response.generations[0].text
+
+        patterns.append({"log_message": message, "patterns": generated_patterns})
+
+    return patterns
+
+
+# Generate patterns for each cluster
+cluster_patterns = generate_patterns(cluster_messages)
+
+# Print the identified patterns (top keywords for each cluster)
+print("Identified Patterns:")
+for cluster_id, patterns in enumerate(cluster_patterns):
+    print(f"Pattern {cluster_id + 1}: {patterns['patterns']}")
