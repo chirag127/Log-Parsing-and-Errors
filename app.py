c1579f3059e14960db5ecbef677c3047b2e2bce2
Chirag <76880977+chirag127@users.noreply.github.com>
2023-10-01 19:43:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 import streamlit as st
-import pandas as pd
+import sqlite3
 import re
 
-# Load the CSV file containing error data (if it exists)
-try:
-    error_database = pd.read_csv("error_database.csv")
-except FileNotFoundError:
-    error_database = pd.DataFrame(columns=["id", "name", "regex", "reason", "solution"])
+# Establish a connection to the SQLite database
+conn = sqlite3.connect("error_database.db")
+cursor = conn.cursor()
+
+# Create a table if it doesn't exist
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS errors (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        regex TEXT,
+        reason TEXT,
+        solution TEXT
+    )
+''')
+conn.commit()
 
 # Function to check if the entered error matches any regex
 def check_error(error_text):
-    for _, row in error_database.iterrows():
-        if re.search(row["regex"], error_text):
-            return (True, row["reason"], row["solution"])
+    cursor.execute("SELECT * FROM errors")
+    rows = cursor.fetchall()
+    for row in rows:
+        if re.search(row[2], error_text):
+            return (True, row[3], row[4])
     return (False, None, None)
 
 # Streamlit app
 st.title("Error Handler")
 
 # User input for error
-user_error = st.text_area("Enter your error message:")
+with st.form(key='error_form'):
+    user_error = st.text_area("Enter your error message:")
+    submit_button = st.form_submit_button("Check Error")
 
-if st.button("Check Error"):
+if submit_button:
     is_known, reason, solution = check_error(user_error)
     if is_known:
         st.success("This is a known error.")
@@ -31,22 +45,17 @@ if st.button("Check Error"):
         st.error("This is a new error.")
         st.write("You can add this error to the database below:")
 
-        # Allow the user to add the error to the CSV database
-        new_error_name = st.text_input("Error Name:")
-        new_error_regex = st.text_input("Error Regex (in a more generalized form):")
-        new_error_reason = st.text_area("Error Reason:")
-        new_error_solution = st.text_area("Error Solution:")
+        # Allow the user to add the error to the database
+        with st.form(key='add_error_form'):
+            new_error_name = st.text_input("Error Name:")
+            new_error_regex = st.text_input("Error Regex (in a more generalized form):")
+            new_error_reason = st.text_area("Error Reason:")
+            new_error_solution = st.text_area("Error Solution:")
+            add_error_button = st.form_submit_button("Add Error to Database")
 
-        if st.button("Add Error to Database"):
-            new_error_id = len(error_database) + 1
-            new_error_data = {
-                "id": new_error_id,
-                "name": new_error_name,
-                "regex": new_error_regex,
-                "reason": new_error_reason,
-                "solution": new_error_solution,
-            }
-            error_database = error_database.append(new_error_data, ignore_index=True)
-            error_database.to_csv("error_database.csv", index=False)
+        if add_error_button:
+            cursor.execute("INSERT INTO errors (name, regex, reason, solution) VALUES (?, ?, ?, ?)",
+                           (new_error_name, new_error_regex, new_error_reason, new_error_solution))
+            conn.commit()
             st.success("Error added to the database.")
             st.text("Refresh the page to check the newly added error.")
