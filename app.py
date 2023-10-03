9c32a46ab83c2fd1611f3c40e6b68805e57975eb
Chirag <76880977+chirag127@users.noreply.github.com>
2023-10-03 09:08:59 +0000

    update 5 files and delete 3 files

--- app.py
+++ app.py
@@ -1,49 +1,123 @@
-from flask import Flask, render_template, request, redirect, url_for
-import pandas as pd
+import streamlit as st
+from pymongo import MongoClient
 import re
+import tomli
 
-app = Flask(__name__)
+# Access MongoDB connection details from Streamlit secrets
+db_uri = st.secrets["db_uri"]
+db_database = st.secrets["db_database"]
+db_collection = st.secrets["db_collection"]
 
-# Load the CSV database containing known error patterns, regex patterns, and solutions
-database_path = "cluster_error_data.csv"
-df = pd.read_csv(database_path)
+# # Connect to MongoDB using configuration
+# client = MongoClient(config["mongodb"]["uri"])
+# db = client[config["mongodb"]["database"]]  # Connect to the specified database
+
+# # Ensure the collection exists, create it if it doesn't
+# collection_name = config["mongodb"]["collection"]
+# if collection_name not in db.list_collection_names():
+#     db.create_collection(collection_name)
+
+# collection = db[collection_name]
+
+# Connect to MongoDB using secrets
+client = MongoClient(db_uri)
+
+
+dblist = myclient.list_database_names()
+print("the list of databases are: ", dblist)
+
+if db_database in dblist:
+  print("The database exists.")
+
+  db = client[db_database]  # Connect to the specified database
+
+else:
+  print("The database does not exist.")
+
+  db = client[db_database]  # Connect to the specified database
+
+
+# Ensure the collection exists, create it if it doesn't
+collection_name = db_collection
+if collection_name not in db.list_collection_names():
+    db.create_collection(collection_name)
+
+collection = db[collection_name]
+
+
 
 def check_known_errors(user_input):
-    # Check if the user input matches known error patterns using regex
+    # Check if the regex pattern matches any document in the MongoDB collection
     matched_errors = []
-    for index, row in df.iterrows():
-        regex_pattern = row["Error Regex"]
+    for error_doc in collection.find():
+        regex_pattern = error_doc["ErrorRegex"]
         if re.search(regex_pattern, user_input, re.IGNORECASE):
-            matched_errors.append(row["Error Solution"])
+            matched_errors.append(error_doc["ErrorSolution"])
     return matched_errors
 
 def add_new_error(user_input, solution):
-    # Implement the logic to add the new error and its solution to the database here
-    # Update the CSV file with the new entry
-    new_entry = {"Error Regex": user_input, "Error Solution": solution}
-    global df
-    df = pd.concat([df, pd.DataFrame([new_entry])], ignore_index=True)
-    df.to_csv(database_path, index=False)
+    # Insert a new document into the MongoDB collection
+    error_doc = {"ErrorRegex": user_input, "ErrorSolution": solution}
+    collection.insert_one(error_doc)
 
-@app.route("/", methods=["GET", "POST"])
-def home():
-    if request.method == "POST":
-        user_input = request.form["user_input"]
-        matched_errors = check_known_errors(user_input)
 
-        if matched_errors:
-            return render_template("result.html", matched_errors=matched_errors)
-        else:
-            return render_template("add_error.html", user_input=user_input)
+def if_submit_button_clicked(new_error_input, new_error_solution):
+    if new_error_input and new_error_solution:
+        add_new_error(new_error_input, new_error_solution)
+        st.success("New error added to the database!")
 
-    return render_template("index.html")
+def main():
+    # Streamlit app
+    st.set_page_config(
+        page_title="Error Message Analyzer",
+        page_icon="🚀",
+    )
 
-@app.route("/add_error", methods=["POST"])
-def add_error():
-    user_input = request.form["user_input"]
-    solution = request.form["solution"]
-    add_new_error(user_input, solution)
-    return redirect(url_for("home"))
+    # Containers
+    top = st.container()
+
+    # User input for error message
+    with top:
+        st.title("Error Message Analyzer")
+
+        if "user_input" not in st.session_state:
+            st.session_state.user_input = ""
+
+        st.text_area(
+            "Enter your error message:", st.session_state.user_input, key="user_input"
+        )
+
+        if st.button("Check Error") or st.session_state.user_input:
+            matched_errors = check_known_errors(st.session_state.user_input)
+            if matched_errors:
+                st.success("Known Error Detected")
+                for error_solution , i in zip(matched_errors, range(len(matched_errors))):
+
+                    # Display the error solution in a good format
+                    st.write(f"Solution {i+1}:" , error_solution)
+            else:
+                st.header("New Error Detected")
+                st.write("This error is not in the database. Would you like to add it?")
+
+                if "new_error_input" not in st.session_state:
+                    st.session_state.new_error_input = ""
+                if "new_error_solution" not in st.session_state:
+                    st.session_state.new_error_solution = ""
+
+                new_error_input = st.text_input(
+                    "Error Regex (Edit if necessary):",
+                    st.session_state.user_input,
+                    key="new_error_input",
+                )
+                new_error_solution = st.text_input(
+                    "Error Solution (Provide a solution for the error):",
+                    st.session_state.new_error_solution,
+                    key="new_error_solution",
+                )
+                if st.button("Submit"):
+                    if_submit_button_clicked(
+                        new_error_input, new_error_solution
+                    )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
--- md.py
+++ md.py
@@ -3,44 +3,30 @@ import pandas as pd
 # Define the error examples, regex patterns, and error solutions for each cluster
 cluster_data = [
     {
-        "Cluster": 1,
-        "Error Example": "a cookie header was received [${jndi:ldap://log4shell-generic-8molyf0ab2aqtscsyugh${lower:ten}.w.nessus.org/nessus}=${jndi:ldap://log4shell-generic-8molyf0ab2aqtscsyugh${lower:ten}.w.nessus.org/nessus};]",
         "Error Regex": r"cookie header was received.*invalid cookie",
         "Error Solution": "Ignore the invalid cookie in the received header.",
     },
     {
-        "Cluster": 2,
-        "Error Example": "servlet.service() for servlet [dispatcherservlet] in context with path [] threw exception",
         "Error Regex": r"servlet.service\(\) for servlet \[dispatcherservlet\] threw exception",
         "Error Solution": "Handle the exception thrown by the servlet service.",
     },
     {
-        "Cluster": 3,
-        "Error Example": "discoveryclient_rdd-async-service/p1049433.prod.cloud.fedex.com:rdd-async-service:6521 - was unable to send heartbeat!",
         "Error Regex": r"discoveryclient.*unable to send heartbeat",
         "Error Solution": "Investigate and resolve the issue related to sending heartbeat.",
     },
     {
-        "Cluster": 4,
-        "Error Example": "exception processing errorpage[errorcode=0, location=/error]",
         "Error Regex": r"exception processing errorpage\[errorcode=\d+.*location=\/error\]",
         "Error Solution": "Handle the exception in the error processing code.",
     },
     {
-        "Cluster": 5,
-        "Error Example": 'request execution failed with message: i/o error on put request for "": connect to rddgwslas03.prod.cloud.fedex.com:8094 [rddgwslas03.prod.cloud.fedex.com/10.228.54.50] failed: connection refused (connection refused); nested exception is org.apache.http.conn.httphostconnectexception: connect to rddgwslas03.prod.cloud.fedex.com:8094 [rddgwslas03.prod.cloud.fedex.com/10.2',
         "Error Regex": r'request execution failed with message: i/o error on put request for "".*connect to .* failed: connection refused',
         "Error Solution": "Investigate the connection issue to the specified host.",
     },
     {
-        "Cluster": 6,
-        "Error Example": 'request execution failed with message: i/o error on get request for "": connect to rddgwsatl04.prod.cloud.fedex.com:8094 [rddgwsatl04.prod.cloud.fedex.com/10.52.77.32] failed: connection refused (connection refused); nested exception is org.apache.http.conn.httphostconnectexception: connect to rddgwsatl04.prod.cloud.fedex.com:8094 [rddgwsatl04.prod.cloud.fedex.com/10.52.77.32] failed: connection refused (connection refused)',
         "Error Regex": r'request execution failed with message: i/o error on get request for "".*connect to .* failed: connection refused',
         "Error Solution": "Investigate the connection issue to the specified host.",
     },
     {
-        "Cluster": 7,
-        "Error Example": 'request execution failed with message: i/o error on put request for "": connect to rddgwsatl02.prod.cloud.fedex.com:8094 [rddgwsatl02.prod.cloud.fedex.com/10.52.172.30] failed: connection refused (connection refused); nested exception is org.apache.http.conn.httphostconnectexception: connect to rddgwsatl02.prod.cloud.fedex.com:8094 [rddgwsatl02.prod.cloud.fedex.com/10.5',
         "Error Regex": r'request execution failed with message: i/o error on put request for "".*connect to .* failed: connection refused',
         "Error Solution": "Investigate the connection issue to the specified host.",
     },
